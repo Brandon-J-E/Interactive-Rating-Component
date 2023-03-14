0abfdf8a6d3d2c818d9ed84a5e7fe9e5ab120a85
Brandon Erasmus <brandonj.erasmus@gmail.com>
2023-03-14 21:03:52 +0200

    Made changes to the rating system do display the number in numbers and not the word.

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const urlParams = new URLSearchParams(location.search);
 const rating = urlParams.get('rating');
 
   window.addEventListener('DOMContentLoaded', function() {
-    const ratingElement = document.getElementById('ratingElement').innerHTML = `You selected ${rating} out of five.`;
+    const ratingElement = document.getElementById('ratingElement').innerHTML = `You selected ${rating} out of 5.`;
     ratingElement.textContent = localStorage.getItem('ratingElement');
   });
